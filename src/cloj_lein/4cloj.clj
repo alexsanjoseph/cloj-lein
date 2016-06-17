b12e99763d54f77768023ee540b5fa7bc87eb6e7
alexsanjoseph <alexsanjoseph@gmail.com>
2016-06-17 22:19:42 +0530

    [4cloj] 51 - 53

--- src/cloj_lein/4cloj.clj
+++ src/cloj_lein/4cloj.clj
@@ -172,3 +172,19 @@
 
 ;; 50. Split by type
 (#(vals (group-by type %)) [1 :a 2 :b 3 :c])
+
+;; 51. Advanced destructuring
+[1 2 3 4 5]
+
+;; 52. Intro to destructuring
+[c e]
+
+;; 53. Longest increasing subsequence
+(def x [5 6 1 3 2 7])
+(def x [1 2 2 3 4 5])
+
+(fn [x]
+  (map > ()))
+(identity x)
+
+(partition-by identity (map > (drop 1 x) (drop-last 1 x)))  
